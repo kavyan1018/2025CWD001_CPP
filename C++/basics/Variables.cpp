66b3cdc3e100811678ee9cbb4c035f2088771770
Kavyan Nayak <kavyannayak104@gmail.com>
2025-12-11 18:28:59 +0530

    Variable

---
+++ C++/basics/Variables.cpp
@@ -0,0 +1,26 @@
+// int , char , float , double, string , bool
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int MyNum = 5;
+
+    double floatnum = 5.99;
+
+    char Letter = 'K';
+
+    string myText = "Hello";
+
+    bool myBoolean = "true";
+
+
+    cout << "Integer :- " << MyNum << endl;
+    cout << "Double :- " << floatnum << endl;
+    cout << "Character :- " << Letter << endl;
+    cout << "String :- " << myText << endl;
+    cout << "Boolean :- " << myBoolean  << endl;
+}
